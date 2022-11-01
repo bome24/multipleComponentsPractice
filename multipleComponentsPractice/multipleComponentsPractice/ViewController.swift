c45381e5f5b58d34de4cded9a1a632dc786d38a5
bome24 <81296575+bome24@users.noreply.github.com>
2022-11-01 23:21:52 +0900

    [ADD] #1 add multiple components
    mutiple components on different view

---
+++ multipleComponentsPractice/multipleComponentsPractice/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  multipleComponentsPractice
+//
+//  Created by BoMin on 2022/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var componentsView: UIView!
+    
+    @IBOutlet weak var wallDesign1: UIButton!
+    @IBOutlet weak var wallDesign2: UIButton!
+    @IBOutlet weak var wallDesign3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
